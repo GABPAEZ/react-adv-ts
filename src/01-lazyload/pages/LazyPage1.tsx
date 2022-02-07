cdb7d09a6b00e3860e502869aec7e0b342a56e3b
Gabriel Paez <gabpaez@gmail.com>
2022-02-07 18:52:16 -0300

    Lazyload basico listo

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -4,3 +4,4 @@ export const LazyPage1 = () => {
   return <h1>LazyPage1</h1>;
 };
 
+export default LazyPage1;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,51 +1,48 @@
+import { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink,
+  Redirect,
 } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 // REACT ROUTER DOM VERSION 6
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="react-logo" />
-          <ul>
-            <li>
-              <NavLink to="/lazy1" activeClassName="nav-active" exact>
-                Lazy1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" activeClassName="nav-active" exact>
-                Lazy2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" activeClassName="nav-active" exact>
-                Lazy3
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/lazy1">
-            <LazyPage1 />
-          </Route>
-          <Route path="/lazy2">
-            <LazyPage2 />
-          </Route>
-          <Route path="/lazy3">
-            <LazyPage3 />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Suspense fallback={<span>Loading...</span>}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="react-logo" />
+            <ul>
+              {/* route se desectructura en patch y name */}
+              {routes.map(({ path, name }) => (
+                <li key={path}>
+                  <NavLink to={path} activeClassName="nav-active" exact>
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Switch>
+            {/* route se desectructura en path y Component */}
+            {routes.map(({ path, Component }) => (
+              <Route
+                path={path}
+                key="{path}"
+                render={() => <Component />}
+              ></Route>
+            ))}
+            <Redirect to={routes[0].path} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 };
